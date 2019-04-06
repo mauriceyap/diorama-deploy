ed8a1972f29f1cc5f8b736a729a430524e23dc54
Maurice Yap <mauriceyap@hotmail.co.uk>
2019-04-06 22:37:36 +0100

    use correct inventory file

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ USER_ACCOUNT=$2
 
 echo $IP_ADDRESS ansible_connection=ssh ansible_ssh_user=$USER_ACCOUNT ansible_python_interpreter=/usr/bin/python3 > inventory
 
-ansible-playbook -i inventory-test -v -e "linux_user=$USER_ACCOUNT" playbook.yml
+ansible-playbook -i inventory -v -e "linux_user=$USER_ACCOUNT" playbook.yml
