ea1d512d6ded9fe690407132b7e7a5b3dde1f56a
Maurice Yap <mauriceyap@hotmail.co.uk>
2019-06-04 13:11:46 +0100

    use local ansible path

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ USER_ACCOUNT=$2
 
 echo $IP_ADDRESS ansible_connection=ssh ansible_ssh_user=$USER_ACCOUNT ansible_python_interpreter=/usr/bin/python3 > inventory
 
-ansible-playbook -i inventory -v -e "linux_user=$USER_ACCOUNT" playbook.yml
+~/.local/bin/ansible-playbook -i inventory -v -e "linux_user=$USER_ACCOUNT" playbook.yml
